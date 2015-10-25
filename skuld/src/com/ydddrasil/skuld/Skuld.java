030de52fa7d4cccf394336149cc33e7407196a1e
trole <teroleg@gmail.com>
2015-10-25 23:47:30 +0200

    reinit modules

---
+++ skuld/src/com/ydddrasil/skuld/Skuld.java
@@ -0,0 +1,15 @@
+package com.ydddrasil.skuld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Skuld extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
